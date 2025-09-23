1bd1a5a20b96be95309805f1d2fec9d7de0fca2b
Matvey <mateusru54063@gmail.com>
2025-09-23 14:56:31 +0300

    Added python-dotenv n modificated manage.py

--- OneHeroRush/manage.py
+++ OneHeroRush/manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from pathlib import Path
+import dotenv
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OneHeroRush.settings')
+    dotenv_path = Path(__file__).resolve().parent.parent / '.env'
+    dotenv.load_dotenv(dotenv_path=dotenv_path)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
