7775c5b111a5c61eabca242d78bd671a308c1a6e
Matvey <mateusru54063@gmail.com>
2025-09-26 11:01:01 +0300

    new apps in INSTALLED_APPS n in urls

--- OneHeroRush/config/urls.py
+++ OneHeroRush/config/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('api/', include('Users.urls')), 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),  
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), 
+    path('api/', include('Characters.urls')),
+    path('api/', include('Progress.urls')),
+    path('api/', include('inventory.urls')),
 ]
