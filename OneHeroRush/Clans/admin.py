859944c6b4e1a83ff236ceec3996e5c8676a6a4d
Matvey <mateusru54063@gmail.com>
2025-10-06 18:39:00 +0300

    registered models

--- OneHeroRush/Clans/admin.py
+++ OneHeroRush/Clans/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from Clans.models import Clan, ClanMember
 
-# Register your models here.
+
+class ClanMemberInline(admin.TabularInline):
+    model = ClanMember
+    extra = 1
+
+@admin.register(Clan)
+class ClanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'leader', 'members_count', 'total_mmr', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'leader__username')
+    inlines = [ClanMemberInline]
+    readonly_fields = ('created_at',)
+
+@admin.register(ClanMember)
+class ClanMemberAdmin(admin.ModelAdmin):
+    list_display = ('user', 'clan', 'is_officer', 'joined_at')
+    list_filter = ('clan', 'is_officer', 'joined_at')
+    search_fields = ('user__username', 'clan__name')
