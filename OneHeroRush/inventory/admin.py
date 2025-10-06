bd3bd3e2d81cb2f0b4a609ce539c78b1c907fa53
Matvey <mateusru54063@gmail.com>
2025-10-06 18:43:53 +0300

    Registered models.py

--- OneHeroRush/inventory/admin.py
+++ OneHeroRush/inventory/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from inventory.models import Item, Chest
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'level', 'rarity_multiplier', 'created_at')
+    list_filter = ('name', 'rarity_multiplier', 'created_at')
+    search_fields = ('user__username', 'name')
+    readonly_fields = ('created_at', 'stats')
+
+@admin.register(Chest)
+class ChestAdmin(admin.ModelAdmin):
+    list_display = ('user', 'level', 'created_at')
+    list_filter = ('level', 'created_at')
+    search_fields = ('user__username',)
